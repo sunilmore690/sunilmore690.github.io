b4d1a55b65347f7d5b458e4b6fb4be59b7e329d7
Sunil More <sunilmore690@gmail.com>
2018-12-31 12:03:36 +0530

    admin folder added

---
+++ admin/admin.env.js
@@ -0,0 +1,5 @@
+window.env = {
+    // This option can be retrieved in "src/index.js" with "window.env.API_URL".
+    SHOWCASE_API_ENDPOINT: 'https://opt-showcase-api-stage.optcentral.com',
+    OPT_API_ENDPOINT:'https://optqa.optcentral.com'
+  };
